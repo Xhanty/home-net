3d2ea263c4219862c89c8c7820853468f070943b
Santiago Henao <santiagosmithdelgadohenao@gmail.com>
2024-09-08 12:39:55 -0500

    Documentación

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,16 @@
 'use client';
 
+import { useRouter } from 'next/navigation'
+
 export default function Landig() {
+  const history = useRouter();
+
+  // Redirigir al usuario al login
+  history.push('/login');
+
   return (
     <main>
-      <h1>Landing</h1>
+      {/*<h1>Landing</h1>*/}
     </main>
   );
 }
